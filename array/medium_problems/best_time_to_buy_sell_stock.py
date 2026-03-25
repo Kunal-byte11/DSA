86e05d6ff7766e4d8e14523ae669cb8f1874bd9b
Kunal-byte11 <kunaldubey26@outlook.com>
2026-03-25 04:28:39 +0000

    25/3/26

---
+++ array/medium_problems/best_time_to_buy_sell_stock.py
@@ -0,0 +1,14 @@
+# # optimal code
+
+# min_price = 0
+# max_profit = float("inf")
+# n = len(prices)
+
+# for i in range(n):
+#     min_price = min(min_price,prices[i])
+#     max_profit = max(max_profit, prices[i] - min_price)
+
+# return max_profit
+
+# tc=> o(n)
+# sc => o(1)
